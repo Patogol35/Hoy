e172ce9ad3e5c7077a85f382bab2cb684c33d8e7
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-08-26 20:58:01 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,26 +10,36 @@ import { CarritoProvider } from "./context/CarritoContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <AuthProvider>
-                <CarritoProvider>
-                    <Navbar />
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/register" element={<Register />} />
-                        <Route path="/carrito" element={
-                            <ProtectedRoute><Carrito /></ProtectedRoute>
-                        } />
-                        <Route path="/pedidos" element={
-                            <ProtectedRoute><Pedidos /></ProtectedRoute>
-                        } />
-                    </Routes>
-                </CarritoProvider>
-            </AuthProvider>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <CarritoProvider>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/carrito"
+              element={
+                <ProtectedRoute>
+                  <Carrito />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/pedidos"
+              element={
+                <ProtectedRoute>
+                  <Pedidos />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </CarritoProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
