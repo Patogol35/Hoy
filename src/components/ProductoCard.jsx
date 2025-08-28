8d75743acac33d4cfc37410dabeadf4e25478662
Jorge Patricio Santamaría Cherrez <patricio_jorge@hotmail.es>
2025-08-28 18:20:19 -0500

    Actualiza diseño del carrito y corrige errores de producto

--- src/components/ProductoCard.jsx
+++ src/components/ProductoCard.jsx
@@ -2,12 +2,10 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { useCarrito } from "../context/CarritoContext";
 import "../App.css";
-
 export default function ProductoCard({ producto }) {
   const { isAuthenticated } = useAuth();
   const { agregarAlCarrito } = useCarrito();
   const navigate = useNavigate();
-
   const onAdd = async () => {
     if (!isAuthenticated) {
       navigate("/login");
@@ -20,9 +18,24 @@ export default function ProductoCard({ producto }) {
       alert(e.message);
     }
   };
-
   return (
     <div className="card">
+      {/* ✅ Imagen del producto */}
+      {producto.imagen_url && (
+        <img
+  src={producto.imagen_url}
+  alt={producto.nombre}
+  style={{
+    width: '100%',
+    maxWidth: '250px',
+    height: '200px',
+    objectFit: 'cover',
+    borderRadius: '12px',
+    marginBottom: '12px'
+  }}
+/>
+
+      )}
       <h3>{producto.nombre}</h3>
       <p>{producto.descripcion}</p>
       <p className="price">${producto.precio}</p>
