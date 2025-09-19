a12bc8bfaa563c85006785f72f5d3340113e71ed
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-09-19 06:20:58 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,45 +9,42 @@ import { AuthProvider } from "./context/AuthContext";
 import { CarritoProvider } from "./context/CarritoContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Layout from "./components/Layout";
-import { ThemeModeProvider } from "./context/ThemeContext"; // 👈 importamos el provider de tema
 
 function App() {
   return (
-    <ThemeModeProvider>
-      <BrowserRouter>
-        <AuthProvider>
-          <CarritoProvider>
-            <Routes>
-              {/* Todas las rutas con Navbar entran en Layout */}
-              <Route element={<Layout />}>
-                <Route path="/" element={<Home />} />
-                <Route
-                  path="/carrito"
-                  element={
-                    <ProtectedRoute>
-                      <Carrito />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/pedidos"
-                  element={
-                    <ProtectedRoute>
-                      <Pedidos />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route path="/producto/:id" element={<ProductoDetalle />} />
-              </Route>
+    <BrowserRouter>
+      <AuthProvider>
+        <CarritoProvider>
+          <Routes>
+            {/* Todas las rutas con Navbar entran en Layout */}
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />} />
+              <Route
+                path="/carrito"
+                element={
+                  <ProtectedRoute>
+                    <Carrito />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/pedidos"
+                element={
+                  <ProtectedRoute>
+                    <Pedidos />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/producto/:id" element={<ProductoDetalle />} />
+            </Route>
 
-              {/* Estas no muestran Navbar */}
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-            </Routes>
-          </CarritoProvider>
-        </AuthProvider>
-      </BrowserRouter>
-    </ThemeModeProvider>
+            {/* Estas no muestran Navbar */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </CarritoProvider>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
