e955988390c16cf7e951f5d38d385d5efa1c171a
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-09-18 19:12:56 -0500

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import { Box, Container } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
-import Navbar from "./Navbar"; // ajusta la ruta a tu Navbar
+import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
@@ -8,17 +8,13 @@ export default function Layout() {
 
   return (
     <Box>
-      {/* Navbar fijo */}
       <Navbar />
-
-      {/* Contenido principal con ajuste dinámico */}
       <Container
         sx={{
           pt: `calc(${theme.mixins.toolbar.minHeight}px + 16px)`,
           pb: 4,
         }}
       >
-        {/* Aquí se renderizan tus páginas */}
         <Outlet />
       </Container>
     </Box>
