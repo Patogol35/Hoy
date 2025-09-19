27d28f7bc6e011bee2b4d3c875b05d7dd112ed12
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-09-19 06:46:39 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,43 +9,48 @@ import { AuthProvider } from "./context/AuthContext";
 import { CarritoProvider } from "./context/CarritoContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Layout from "./components/Layout";
+import { ThemeModeProvider } from "./context/ThemeContext"; // 👈 importamos el provider de tema
 
 function App() {
-  return (
-    <BrowserRouter>
-      <AuthProvider>
-        <CarritoProvider>
-          <Routes>
-            {/* Todas las rutas con Navbar entran en Layout */}
-            <Route element={<Layout />}>
-              <Route path="/" element={<Home />} />
-              <Route
-                path="/carrito"
-                element={
-                  <ProtectedRoute>
-                    <Carrito />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/pedidos"
-                element={
-                  <ProtectedRoute>
-                    <Pedidos />
-                  </ProtectedRoute>
-                }
-              />
-              <Route path="/producto/:id" element={<ProductoDetalle />} />
-            </Route>
+return (
+<ThemeModeProvider>
+<BrowserRouter>
+<AuthProvider>
+<CarritoProvider>
+<Routes>
+{/* Todas las rutas con Navbar entran en Layout */}
+<Route element={<Layout />}>
+<Route path="/" element={<Home />} />
+<Route
+path="/carrito"
+element={
+<ProtectedRoute>
+<Carrito />
+</ProtectedRoute>
+}
+/>
+<Route
+path="/pedidos"
+element={
+<ProtectedRoute>
+<Pedidos />
+</ProtectedRoute>
+}
+/>
+<Route path="/producto/:id" element={<ProductoDetalle />} />
+</Route>
 
-            {/* Estas no muestran Navbar */}
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </CarritoProvider>
-      </AuthProvider>
-    </BrowserRouter>
-  );
+{/* Estas no muestran Navbar */}  
+          <Route path="/login" element={<Login />} />  
+          <Route path="/register" element={<Register />} />  
+        </Routes>  
+      </CarritoProvider>  
+    </AuthProvider>  
+  </BrowserRouter>  
+</ThemeModeProvider>
+
+);
 }
 
 export default App;
+
