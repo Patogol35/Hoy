82287dd70e7c1698036042fb6f7ee56e5075cb1f
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-09-26 12:56:21 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -93,7 +93,7 @@ export default function Home() {
       </Box>
 
       {/* ================== FILTROS ================== */}
-      <box
+      <paper
         elevation={4}
         sx={{
           p: 3,
@@ -168,7 +168,7 @@ export default function Home() {
           <MenuItem value="asc">Precio: menor a mayor</MenuItem>
           <MenuItem value="desc">Precio: mayor a menor</MenuItem>
         </TextField>
-      </box>
+      </paper>
 
       {/* ================== PRODUCTOS ================== */}
       <Grid container spacing={3} justifyContent="center">
