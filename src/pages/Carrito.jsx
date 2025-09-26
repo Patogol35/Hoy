4515e7273c8a2707c4ad1125785806410fc8a9c2
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-09-25 19:32:16 -0500

    Update Carrito.jsx

--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -77,7 +77,7 @@ export default function Carrito() {
   gutterBottom
   fontWeight="bold"
   align="center"   
-  sx={{ display: "flex", justifyContent: "center", alignItems: "center", gap: 1, color: "primary.main", mt: 2,}}
+  sx={{ display: "flex", justifyContent: "center", alignItems: "center", gap: 1, color: "primary.main", mt: 3,}}
 >
   <ShoppingCartIcon color="primary" sx={{ fontSize: 36 }} />
   Tu Carrito
